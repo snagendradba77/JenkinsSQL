9fd454ba18a1f2980135c84967a2ba6eb6e1242f
Naga <naga@localhost.localdomain>
2025-10-30 22:56:14 +0530

    Create demo table

---
+++ sql/001_create_demo_table.sql
@@ -0,0 +1,34 @@
+-- Create the database if it doesn't exist
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'DemoDB')
+BEGIN
+    CREATE DATABASE DemoDB;
+    PRINT 'Database DemoDB created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'Database DemoDB already exists.';
+END
+GO
+
+-- Switch to the database
+USE DemoDB;
+GO
+
+-- Create a sample table
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Employees')
+BEGIN
+    CREATE TABLE Employees (
+        EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+        FirstName NVARCHAR(50) NOT NULL,
+        LastName NVARCHAR(50) NOT NULL,
+        Email NVARCHAR(100) UNIQUE,
+        HireDate DATE DEFAULT GETDATE()
+    );
+    PRINT 'Table Employees created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'Table Employees already exists.';
+END
+GO
+
