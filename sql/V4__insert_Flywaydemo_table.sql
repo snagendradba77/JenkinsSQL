746da71bd970da27ca3803c2326a22d1eb1f451f
Naga <naga@localhost.localdomain>
2025-11-05 22:26:40 +0530

    insert_Flywaydemo_table.

---
+++ sql/V4__insert_Flywaydemo_table.sql
@@ -0,0 +1,12 @@
+
+Use DemoDB;
+go
+
+INSERT INTO flywaydemo_table2 (name)
+VALUES ('John Doe');
+
+INSERT INTO flywaydemo_table2 (name)
+VALUES ('Alice Smith');
+
+INSERT INTO flywaydemo_table2 (name)
+VALUES ('Bob Johnson'):;
